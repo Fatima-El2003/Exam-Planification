00ca9daa4883bf6d9f413808e5ba436387eda750
FATIMA ELOUAFI <elouafifatima759@gmail.com>
2024-05-07 12:50:24 +0100

    first commit

---
+++ src/main/java/com/ensah/examplan/Config/OurUserInfoDetails.java
@@ -0,0 +1,59 @@
+//package com.ensah.examplan.Config;
+//
+//import com.ensah.examplan.model.Enseignant;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Arrays;
+//import java.util.Collection;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//public class OurUserInfoDetails implements UserDetails {
+//    private String email;
+//    private String password;
+//    //private List<GrantedAuthority> roles;
+//
+//    public OurUserInfoDetails(Enseignant enseignant){
+//        this.email = enseignant.getEmail();
+//        this.password = enseignant.getPassword();
+//        /*this.roles = Arrays.stream(ourUser.getRoles().split(","))
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());*/
+//    }
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return this.password;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return this.email;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
